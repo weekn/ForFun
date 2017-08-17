8f9927e1419650631ea68acb37d5954fb2122a9c
weekn <weekn@qq.com>
2017-08-17 21:56:12 +0800

    change3333

--- pp.php
+++ pp.php
@@ -1,9 +1,9 @@
-<?phpfdsfada
+<?php
 namespace MyProject;
 class Car {
   var $var1;
   var $var2 = "constant string";
-  
+  var $var3=1;
   function func ($arg1, $arg2) {
      print($var1);
 		 print($var2);
